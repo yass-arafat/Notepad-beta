c07ff802b1fc94f3150cdde75a5311605beba909
Yassir Arafat <roni.yassir@gmail.com>
2016-12-30 12:01:18 +0600

    Update FindDialog.java
    Test From yassir

--- npd/src/FindDialog.java
+++ npd/src/FindDialog.java
@@ -8,6 +8,7 @@ import javax.swing.border.*;
 /******************************************************/
 class FindReplaceDemo extends JFrame
 {
+//comment from yassir
 FindDialog dialog=null; 
 JTextArea ta;
 JButton findButton,replaceButton;
